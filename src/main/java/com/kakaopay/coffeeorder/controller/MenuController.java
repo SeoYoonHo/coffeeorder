3c37f8cab4d85c6bb69ea0cf68f1b4f8da1737ad
seoyoonho <gogoy2643@naver.com>
2022-06-24 20:47:51 +0900

    최근 7일 인기메뉴 조회 api 완료

--- src/main/java/com/kakaopay/coffeeorder/controller/MenuController.java
+++ src/main/java/com/kakaopay/coffeeorder/controller/MenuController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kakaopay.coffeeorder.domain.Menu;
+import com.kakaopay.coffeeorder.dto.MostMenuDTO;
 import com.kakaopay.coffeeorder.service.MenuService;
 
 import lombok.AllArgsConstructor;
@@ -16,27 +17,39 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class MenuController {
-	
+
 	private final MenuService menuService;
-	
+
 	@GetMapping("api/v1/menus")
 	public Result<List<MenuDto>> allMenus() {
-		
+
 		List<Menu> menus = menuService.findAllMenus();
-		
+
 		List<MenuDto> collect = menus.stream()
 				.map(m -> new MenuDto(m.getId(), m.getName(), m.getPrice()))
 				.collect(Collectors.toList());
-		
+
 		return new Result<List<MenuDto>>(collect);
 	}
-	
+
+	@GetMapping("api/v1/popularMenu")
+	public Result<List<PopularMenuDto>> popularMenus() {
+		
+		List<MostMenuDTO> popularMenuList = menuService.findPopluarMenus();
+		
+		List<PopularMenuDto> collect = popularMenuList.stream()
+				.map(m -> new PopularMenuDto(m.getId(), m.getName(), m.getPrice(), m.getTotalCnt()))
+				.collect(Collectors.toList());
+		
+		return new Result<List<PopularMenuDto>>(collect);
+	}
+
 	@Data
 	@AllArgsConstructor
 	static class Result<T> {
 		private T data;
 	}
-	
+
 	@Data
 	@AllArgsConstructor
 	static class MenuDto {
@@ -45,5 +58,13 @@ public class MenuController {
 		private int price;
 	}
 	
+	@Data
+	@AllArgsConstructor
+	static class PopularMenuDto {
+		private Long id;
+		private String name;
+		private int price;
+		private Long totalCnt;
+	}
 
 }
